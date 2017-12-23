d92624d3d853dd5a64f6345c4f97663cee635546
phkehl <phkehl@gmail.com>
2017-12-23 22:28:43 +0100

    set version 1.11

--- lib/Foswiki/Plugins/PhotoGalleryPlugin.pm
+++ lib/Foswiki/Plugins/PhotoGalleryPlugin.pm
@@ -74,7 +74,7 @@ use Image::Epeg;
 
 ####################################################################################################
 
-our $VERSION           = '1.11-dev';
+our $VERSION           = '1.11';
 our $RELEASE           = '23 Dec 2017';
 our $SHORTDESCRIPTION  = 'A gallery plugin for JPEG photos from digital cameras.';
 our $NO_PREFS_IN_TOPIC = 1;
