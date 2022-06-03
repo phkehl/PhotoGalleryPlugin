d2f387e9fc23838f2e44d1abb80105477d0f6745
phkehl <phkehl@gmail.com>
2022-06-03 23:45:30 +0200

    cleanup

--- watchlogmin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-tail -F \
-     /home/flip/git/foswiki-dev/core/working/logs/events.log \
-     /home/flip/git/foswiki-dev/core/working/logs/debug.log \
-     /home/flip/git/foswiki-dev/core/working/logs/error.log \
-    | awk '/^==>/ { gsub("^.*/", "", $2); f=$2; getline; } ! /^$/ { print "\033[36m"f"\033[m "$0 }'
